5c70294d1aa59295cdc651e780243e2b9e608a66
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-03-17 22:14:44 +0530

    minor css update

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wallet',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletPage  implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {}
 
+  goBack(){
+    this.router.navigate(['/home'])
+  }
 }
