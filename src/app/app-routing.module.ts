124d77850819c33ca10ea222f4ea02c0388bb5d0
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-06-22 20:34:47 +0530

    added homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginPage } from './login/login.component';
 import { HomePage } from './home/home.page';
-import { QuizPage } from './quiz/quiz.component';
-import { WalletPage } from './wallet/wallet.component';
 
 const routes: Routes = [
   {
@@ -15,16 +13,8 @@ const routes: Routes = [
     component : HomePage
   },
   {
-    path: 'wallet',
-    component : WalletPage
-  },
-  {
-    path: 'quiz/:id',
-    component : QuizPage
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoaderComponent } from './loader/loader.component';
 import { CommonModule } from '@angular/common';
 import { LoginPage } from './login/login.component';
-import { QuizPage } from './quiz/quiz.component';
 import { GoogleadsService } from './googleads.service';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home/home.page';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { WalletPage } from './wallet/wallet.component';
 import { WebService } from './web.service';
 import { interceptorInterceptor } from './interceptor.interceptor';
 import { LoaderService } from './loader.service';
 import { SocketService } from './socket.service';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 @NgModule({
-  declarations: [AppComponent,LoaderComponent,LoginPage,QuizPage,HomePage,WalletPage],
+  declarations: [AppComponent,LoaderComponent,LoginPage,HomePage],
   imports: [CommonModule,BrowserModule,FormsModule,IonicModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
     GoogleadsService,
